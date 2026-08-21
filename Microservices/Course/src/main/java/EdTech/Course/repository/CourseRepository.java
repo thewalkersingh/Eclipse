9d1fbe429009061c15b35bcc6e66a58cbc28ae51
Diwakar <the.walkersingh@hotmail.com>
2026-08-21 13:37:25 +0530

    Updated Drive

--- Microservices/Course/src/main/java/EdTech/Course/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package EdTech.Course.repository;
-
-import EdTech.Course.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    Course findByName(String name);
-    Course findByInstructor(String instructor);
-}
--- Microservices/Course/src/main/java/EdTech/Course/repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package EdTech.Course.repository;
-
-import EdTech.Course.model.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
